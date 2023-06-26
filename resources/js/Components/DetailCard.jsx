671b4f13a13bb7eeca53f0cb1f5bb2fea2796e3c
nathan <kvngnathan8420@gmail.com>
2023-06-26 01:40:35 +0000

    Add user image

--- resources/js/Components/DetailCard.jsx
+++ resources/js/Components/DetailCard.jsx
@@ -22,7 +22,7 @@ const DetailCard = ({ user, chirp }) => {
     const { auth } = usePage().props;
 
     const [editing, setEditing] = useState(false);
-    const [ liked , setLiked ] = useState(false);
+    const [liked, setLiked] = useState(false);
 
     const { data, setData, patch, clearErrors, reset, errors } = useForm({
         message: chirp.message,
@@ -35,42 +35,22 @@ const DetailCard = ({ user, chirp }) => {
         });
     };
 
-    console.log(user.name);
+    const demo = chirp.likes.find((element, index) => {
+        return element.name === user.name;
+    });
 
-   const demo = chirp.likes.find((element, index)=>{
-    return element.name === user.name;
-   })
-
-//    if(demo){
-//     console.log("found");
-//     setLiked(true)
-//    }
-//    else{
-//     console.log("Not found");
-//    }
-
-   useEffect(()=>{
-    demo ? setLiked(true) : setLiked(false)
-   }, [demo])
-
- 
+    useEffect(() => {
+        demo ? setLiked(true) : setLiked(false);
+    }, [demo]);
 
     return (
         <div className="p-6 flex space-x-2 ">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-gray-500 -scale-x-100"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+            <div class=" h-10 w-10">
+                <img
+                    src="/img/anony2.png"
+                    class="object-cover h-10 w-10 rounded-full "
                 />
-            </svg>
+            </div>
             <div className="flex-1">
                 <div className="flex justify-between items-center">
                     <div>
@@ -139,9 +119,7 @@ const DetailCard = ({ user, chirp }) => {
                     </form>
                 ) : (
                     <p className=" mt-4 mb-3 text-lg dark:text-gray-300">
-                        
                         {chirp.message}
-                       
                     </p>
                 )}
 
