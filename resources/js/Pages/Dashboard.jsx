a6bc582dd50f79903da99f470403ed92d949c0bd
nathan <kvngnathan8420@gmail.com>
2023-06-28 18:02:04 +0000

    Add contribution modal

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -13,6 +13,7 @@ import { FaQuestionCircle } from 'react-icons/fa';
 
 //
 import QuestionsModal from '@/Components/QuestionsModal';
+import ContributionModal from '@/Components/ContributionModal';
 
 //
 import { GithubIcon, TwitterIcon, LinkedInIcon } from '@/Components/SocialIcons';
@@ -22,6 +23,7 @@ import { GithubIcon, TwitterIcon, LinkedInIcon } from '@/Components/SocialIcons'
 export default function Dashboard({ auth }) {
 
     const [ openQuestionModal , setOpenQuestionModal ] = useState(false);
+    const [ openContributionModal , setOpenContributionModal ] = useState(false)
 
     const openingModel = (e) => {
 
@@ -29,11 +31,20 @@ export default function Dashboard({ auth }) {
         setOpenQuestionModal(true);
     }
 
-    //
+    //close Question modal
     const closeQuestionModel = () => {
-        setOpenQuestionModal(false);
-         
-        
+        setOpenQuestionModal(false);     
+    }
+
+    //
+    const handleContributionModal = (e) => {
+        e.preventDefault();
+        setOpenContributionModal(true);
+    }   
+
+    //
+    const closeContributionModal = () =>{
+        setOpenContributionModal(false);
     }
 
 
@@ -72,7 +83,7 @@ export default function Dashboard({ auth }) {
                 </div>
 
                 <div className="max-w-7xl mx-auto px-5 sm:px-6 lg:px-8">
-                <Link href={route('posts.index')}>
+                <Link href="#" onClick={handleContributionModal}>
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm rounded-lg flex gap-5 p-5 items-center">
                     <div><FaQuestionCircle size={30} className='text-red-500'/></div>
                         <div className=" text-gray-900 dark:text-gray-100">
@@ -89,20 +100,43 @@ export default function Dashboard({ auth }) {
             <QuestionsModal show={openQuestionModal} onClose={closeQuestionModel} >
                 <div className='p-6'>
                     <div className=' flex flex-col items-center justify-center'>
-                        <h2 className=' text-2xl font-bold dark:text-gray-200 pb-2'>About us</h2>
+                        <h2 className=' text-2xl font-bold dark:text-gray-200 pb-2'><span>&#128220; </span>About us</h2>
                         <p className=' text-lg text-gray-500 p-2 pb-3'>Visit our social pages for more info on our development team</p>
                         <div className=' flex justify-center items-center gap-5  py-3 px-2'>
                             <Link><GithubIcon /></Link>
                             <Link><TwitterIcon/></Link>
                             <Link><LinkedInIcon/></Link>
                             
-                            
-
                         </div>
                     </div>
                 </div>
 
             </QuestionsModal>
+
+            <ContributionModal show={openContributionModal} onClose={closeContributionModal}>
+            <div className='p-6'>
+                    <div className=' flex flex-col items-center justify-center'>
+                        <div>
+                        <h2 className=' text-2xl font-bold dark:text-gray-200 pb-2 text-center uppercase'><span>&#128220; </span>Contribution</h2>
+                        <p className=' text-lg text-gray-500 p-2'> To sponser or donate to the Pycode team </p>
+                        <p className='p-2 dark:text-gray-300'>Send to : <span>0558218741</span></p>
+                        </div>
+                        <div className='py-2'>
+                            <h2 className='font-bold uppercase text-lg dark:text-gray-200 py-2'><span>&#x1F468;&#x200D;&#x1F4BB; </span>Technical Support</h2>
+                            <div className='px-2'>
+                            <p className='dark:text-gray-400 mb-2'>Found a bug ?</p>
+                            <p className=' dark:text-gray-400'>Want to contribute to adding a new feature ?</p>
+                            </div>
+                            <p>icon</p>
+                            
+                        </div>
+                       
+                    </div>
+                </div>
+
+            </ContributionModal>
+
+
         </AuthenticatedLayout>
     );
 }
