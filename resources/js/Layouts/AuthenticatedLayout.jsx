0dd869ede2a87603d18fe9d51271b4dfbbbe6e38
nathan <kvngnathan8420@gmail.com>
2023-06-28 10:56:55 +0000

    profile view update

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -137,8 +137,8 @@ export default function Authenticated({ user, header, children }) {
             </nav>
 
             {header && (
-                <header className="bg-white dark:bg-gray-800 shadow">
-                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
+                <header className="bg-white dark:bg-gray-800 shadow mt-20">
+                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 uppercase">{header}</div>
                 </header>
             )}
 
