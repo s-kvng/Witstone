e8cda47db1f6f6907bf2e96ae0ce9a6bfbad02b4
nathan <kvngnathan8420@gmail.com>
2023-06-30 10:33:09 +0000

    Updates

--- resources/js/Components/Card.jsx
+++ resources/js/Components/Card.jsx
@@ -50,7 +50,10 @@ const Card = ({ chirp }) => {
     return (
         <div className="p-6 flex space-x-2 ">
             <div className=" h-10 w-10">
-                <img src="/img/anony2.png" className="object-cover h-10 w-10 rounded-full " />
+                <img
+                    src="/img/anony2.png"
+                    className="object-cover h-10 w-10 rounded-full "
+                />
             </div>
             <div className="flex-1">
                 <div className="flex justify-between items-center">
@@ -123,14 +126,16 @@ const Card = ({ chirp }) => {
                         <Link href={route("posts.show", chirp.id)}>
                             {truncatedText}
 
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-4 w-4 text-gray-400"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                            </svg>
+                            {truncatedText.length === 50 && (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-4 w-4 text-gray-400 inline"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                </svg>
+                            )}
                         </Link>
                     </p>
                 )}
@@ -163,25 +168,24 @@ const Card = ({ chirp }) => {
                     </p>
                     <p className="flex align-items-center">
                         <Link href={route("posts.show", chirp.id)}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6 text-gray-600 -scale-x-100"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                            />
-                        </svg>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6 text-gray-600 -scale-x-100"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                                />
+                            </svg>
                         </Link>
                         <span className="dark:text-zinc-400 text-zinc-600">
                             {chirp.commentCount}
                         </span>
-                        
                     </p>
                 </div>
             </div>
