20dc4f598c70274d09d5f925e6a725bbed9167b7
nathan <kvngnathan8420@gmail.com>
2023-07-16 13:18:03 +0000

    Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,10 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+        <link rel="manifest" href="/img/site.webmanifest">
         <!-- <link
             rel="stylesheet"
             href="https://twemoji.maxcdn.com/v/latest/twemoji.css"
