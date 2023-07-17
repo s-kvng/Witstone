2d634c2a41e69183f32d29325c8f37057ff7b6d3
nathan <kvngnathan8420@gmail.com>
2023-07-17 00:19:41 +0000

    Add counter for post

--- resources/js/Pages/Post/Form.jsx
+++ resources/js/Pages/Post/Form.jsx
@@ -6,6 +6,7 @@ import { useForm, Head } from "@inertiajs/react";
 
 export default function Index({ auth }) {
     const [disable, setDisable] = useState(false);
+    const [counter, setCounter] = useState(300);
 
     const { data, setData, post, processing, reset, errors } = useForm({
         message: "",
@@ -19,6 +20,7 @@ export default function Index({ auth }) {
     //disable btn if user types more than 255 words
     useEffect(() => {
         data.message.length > 300 ? setDisable(true) : setDisable(false);
+        setCounter(300 - data.message.length);
     }, [data.message]);
 
     return (
@@ -32,16 +34,28 @@ export default function Index({ auth }) {
                             value={data.message}
                             placeholder="What's on your mind?"
                             rows={`5`}
-                            className="block w-full dark:bg-gray-900 dark:text-white border-gray-300 dark:border-gray-500 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-40 rounded-md shadow-sm shadow-zinc-300"
+                            className="block w-full mb-2 dark:bg-gray-900 dark:text-white border-gray-300 dark:border-gray-500 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-40 rounded-md shadow-sm shadow-zinc-300"
                             onChange={(e) => setData("message", e.target.value)}
                         ></textarea>
                         <InputError message={errors.message} className="mt-2" />
-                        <PrimaryButton
-                            className="mt-4 bg-sky-500 dark:bg-sky-700 dark:text-white dark:focus:bg-sky-600 dark:hover:bg-sky-600"
-                            disabled={processing || disable}
-                        >
-                            Post
-                        </PrimaryButton>
+                        <div className=" flex justify-between items-center">
+                            <PrimaryButton
+                                className=" bg-sky-500 dark:bg-sky-700 dark:text-white dark:focus:bg-sky-600 dark:hover:bg-sky-600"
+                                disabled={processing || disable}
+                            >
+                                Post
+                            </PrimaryButton>
+
+                            <p
+                                className={` dark:text-white text-slate-900/90 border ${
+                                    counter < 0
+                                        ? "border-red-500 bg-red-400"
+                                        : "border-blue-500 bg-blue-400"
+                                } h-9 min-h-9 max-h-9 w-9 min-w-9 max-w-9 flex justify-center items-center p-3 rounded-full shadow-sm`}
+                            >
+                                {counter}
+                            </p>
+                        </div>
                     </form>
                 </div>
 
